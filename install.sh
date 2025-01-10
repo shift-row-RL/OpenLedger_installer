832d9c0a3de198ab16e89d3977c0768deb3f3ab6
shift-row-RL <182500980+shift-row-RL@users.noreply.github.com>
2025-01-10 14:46:15 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,9 +3,11 @@ sudo apt install -y apt-transport-https ca-certificates curl software-properties
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
+sudo systemctl start docker
+sudo systemctl enable docker
 sudo apt install -y docker-ce docker-ce-cli containerd.io
-wget https://cdn.openledger.xyz/openledger-node-1.0.0-linux.zip 
-unzip openledger-node-1.0.0-linux.zip
+sudo wget https://cdn.openledger.xyz/openledger-node-1.0.0-linux.zip
+sudo unzip openledger-node-1.0.0-linux.zip
 sudo dpkg -i openledger-node-1.0.0.deb
 sudo apt-get install desktop-file-utils
-openledger-node --no-sandbox
+sudo openledger-node --no-sandbox
